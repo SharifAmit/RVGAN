eb245c6f469755a6c575e9592e148a4fd8d92d6f
Sharif Amit Kamran <SharifAmit@users.noreply.github.com>
2021-05-18 10:24:22 -0700

    Update losses.py

--- src/losses.py
+++ src/losses.py
@@ -21,13 +21,3 @@ def weighted_feature_matching_loss(y_true, fake_samples, image_input, real_sampl
             fm_loss += inner_weight[1] * K.mean(K.abs(y_fake[i] - y_real[i]))
     fm_loss *= sample_weight
     return fm_loss
-def feature_matching_loss(y_true, fake_samples, image_input, real_samples, D,
-                          sample_weight):
-    y_fake = D([image_input, fake_samples])[1:]
-    y_real = D([image_input, real_samples])[1:]
-
-    fm_loss = 0
-    for i in range(len(y_fake)):
-        fm_loss += K.mean(K.abs(y_fake[i] - y_real[i]))
-    fm_loss *= sample_weight
-    return fm_loss
