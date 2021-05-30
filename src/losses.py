a1182570bcbb249ff558f0b440940dc4621c8f55
Sharif Amit Kamran <SharifAmit@users.noreply.github.com>
2021-05-30 16:01:59 -0700

    Update losses.py

--- src/losses.py
+++ src/losses.py
@@ -1,13 +1,6 @@
 import keras.backend as K
 
-def hinge_loss_discriminator(y_true,y_pred):
-    real_loss = K.mean(K.relu(1.0 - y_true))
-    fake_loss = K.mean(K.relu(1.0 + y_pred))
-    loss = real_loss + fake_loss
-    return loss
-def hinge_loss_generator(y_true,y_pred):
-    fake_loss = -1*K.mean(y_pred)
-    return fake_loss
+
 def weighted_feature_matching_loss(y_true, fake_samples, image_input, real_samples, D, inner_weight, 
                           sample_weight):
     y_fake = D([image_input, fake_samples])[1:]
