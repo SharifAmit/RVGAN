20c783cd1efbe4eafd806732ebbe4e8e7d059d33
Sharif Amit Kamran <SharifAmit@users.noreply.github.com>
2021-10-21 07:11:43 -0700

    Update losses.py

--- src/losses.py
+++ src/losses.py
@@ -4,5 +4,5 @@ import keras.backend as K
 def weighted_feature_matching_loss(y_true, y_pred):
     fm_loss = 0
     for i in range(len(y_true)):
-        fm_loss += K.mean(K.abs(y_true[i] - y_pred[i]))
+        fm_loss += 0.5*K.mean(K.abs(y_true[i] - y_pred[i]))
     return fm_loss
